f25a15a12712b9060aff03d8fb78e3833478a203
Lois Kilberg <46911988+loiskilberg@users.noreply.github.com>
2019-02-23 18:54:19 -0500

    update name

--- source/conf.py
+++ source/conf.py
@@ -6,9 +6,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Sample Resume with Sphinx'
-copyright = u'2019, Mark Hoeber'
-author = u'Mark Hoeber'
+project = u'Creating Spiral Notebooks'
+copyright = u'2019, Lois Kilberg'
+author = u'Lois Kilberg'
 
 
 #version = u'1.0'
