f27e4dcfcbb2d8e364888dfe22953374e5a2e484
Lois Kilberg <46911988+loiskilberg@users.noreply.github.com>
2019-02-23 18:17:31 -0500

    add files manually from Getting Started w/ Sphinx

---
+++ source/conf.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+# 
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+# General information about the project.
+project = u'Sample Resume with Sphinx'
+copyright = u'2019, Mark Hoeber'
+author = u'Mark Hoeber'
+
+
+#version = u'1.0'
+# The full version, including alpha/beta/rc tags.
+#release = u'1.0'
+
+
+language = None
+
+
+pygments_style = 'sphinx'
+
+#Here's where to find some themes: https://www.sphinx-doc.org/en/master/usage/theming.html?highlight=theme
+#html_theme = 'default'
+#html_theme = 'agogo'
+#html_theme = 'alabaster'
+#html_theme = 'bizstyle'
+#html_theme = 'classic'
+#html_theme = 'haiku'
+#html_theme = 'nature'
+#html_theme = 'pyramid'
+#html_theme = 'scrolls'
+#html_theme = 'sphinxdoc'
+html_theme = 'traditional'
+
+rst_epilog = """
+.. include:: substitutions.txt
+"""
+
+
+
+
+
---
+++ source/index.rst
@@ -0,0 +1,23 @@
+Lois Kilberg
+############
+
+
+.. image:: images/squareCroppedOptionJustHead.jpg
+         :name: photo of Lois
+         :width: 30%
+
+Technical Writer
+
+|Contents|
+
+.. toctree::
+   :maxdepth: 2
+
+   contactInfo
+   profile
+   experience
+   skills
+   education
+
+
+.. add new files to list above to include them in toc
