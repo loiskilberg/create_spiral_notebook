b42904826a587f3a03b715866f1637bd55f2f4e0
Lois Kilberg <46911988+loiskilberg@users.noreply.github.com>
2019-02-23 18:37:37 -0500

    start with nature theme

--- source/conf.py
+++ source/conf.py
@@ -28,11 +28,11 @@ pygments_style = 'sphinx'
 #html_theme = 'bizstyle'
 #html_theme = 'classic'
 #html_theme = 'haiku'
-#html_theme = 'nature'
+html_theme = 'nature'
 #html_theme = 'pyramid'
 #html_theme = 'scrolls'
 #html_theme = 'sphinxdoc'
-html_theme = 'traditional'
+#html_theme = 'traditional'
 
 rst_epilog = """
 .. include:: substitutions.txt
