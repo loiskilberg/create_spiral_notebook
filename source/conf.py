d60021b05a4c9981ee374ee4e115cde7e6094b26
Lois Kilberg <46911988+loiskilberg@users.noreply.github.com>
2019-03-06 07:22:26 -0500

    temporarily comment out color changes

--- source/conf.py
+++ source/conf.py
@@ -34,9 +34,9 @@ html_theme = 'classic'
 #html_theme = 'sphinxdoc'
 #html_theme = 'traditional'
 
-html_theme_options = {
-    "sidebarbgcolor": "#343261",
-}
+#html_theme_options = {
+#    "sidebarbgcolor": "#343261",
+#}
 
 html_logo = "images/logo_myPSDfileWithLLC_width200px.png"
 
